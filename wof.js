d36bb46fd6c4e101fa28f3fd9e4208b2ce935f0f
Andrew Littlefield <andrew.ray.allan@gmail.com>
2017-06-16 15:46:55 -0600

    merge two branches

--- wof.js
+++ wof.js
@@ -8,6 +8,12 @@ $(document).ready(function() {
 	let hide = $()
 	let game = new Game();
 	game.wordBox();
+	$('.letters').click(function() {
+		let clickedLetter = $(this).attr('id');
+		game.letterSelect(clickedLetter);
+		$(this).css({'color':'red'});
+	});
+
 
 
 
@@ -15,7 +21,7 @@ $(document).ready(function() {
 });
 
 function Game() {
-	this.wordArray = ['Aragorn', 'Halo', 'Kangaroo', 'Purple', 'Firework'];
+	this.wordArray = ['ARAGORN', 'HALO', 'KANGAROO', 'PURPLE', 'FIREWORK'];
 	self = this
 	this.randomWord = function() {
 		return this.wordArray[Math.floor(Math.random() * this.wordArray.length)];
@@ -23,21 +29,20 @@ function Game() {
 	this.wordBox = function() {
 		let rando = self.randomWord();
 		for (var i = 0; i < rando.length; i++) {
-			$('#box1').data('data-letter', rando[0]);
-			$('#box2').data('data-letter', rando[1]);
-			$('#box3').data('data-letter', rando[2]);
-			$('#box4').data('data-letter', rando[3]);
-			$('#box5').data('data-letter', rando[4]);
-			$('#box6').data('data-letter', rando[5]);
-			$('#box7').data('data-letter', rando[6]);
-			$('#box8').data('data-letter', rando[7]);
-			$('#box9').data('data-letter', rando[8]);
-			$('#box10').data('data-letter', rando[9]);
-			$('#box11').data('data-letter', rando[10]);
-			$('#box12').data('data-letter', rando[11]);
+			$('#box' + (i + 1)).data('.letter', rando[i]);
+			console.log(rando);
 		}
 	};
-	
+	this.letterSelect = function(clickedLetter) {
+		let letterBox = $('.letter-box');
+		letterBox.each(function () {
+			if($(this).data('.letter') == clickedLetter) {
+				$(this).html(clickedLetter);
+			}
+		}); 
+		let dataLetter = $('.letter-box').data('.letter');
+
+	};
 
 
 
